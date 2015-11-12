4af3a2b96f2334b0923f877fe4da4ff4d793c432
Kiran Challa <kichalla@microsoft.com>
2015-11-12 10:23:58 -0800

    after copying the source files and making very few modifications

---
+++ src/System.Configuration/ConfigurationAllowDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Configuration
+{
+
+    public enum ConfigurationAllowDefinition
+    {
+        MachineOnly = 0,
+        MachineToWebRoot = 100,
+        MachineToApplication = 200,
+        Everywhere = 300,
+    }
+}
---
+++ src/System.Configuration/ConfigurationAllowExeDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Configuration
+{
+
+    public enum ConfigurationAllowExeDefinition
+    {
+        MachineOnly = 0,
+        MachineToApplication = 100,
+        MachineToRoamingUser = 200,
+        MachineToLocalUser = 300
+    }
+}
